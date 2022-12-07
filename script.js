cbe08c9626ea7a3eb03e051e6b3077d34a598d3e
Circulr <ryan0702.rw@gmail.com>
2022-12-07 17:27:05 +0700

    new

---
+++ script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if(this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if(this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  })
+
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({scrollTop: 0});
+  })
+  
+  $('.menu-btn').click(function() {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  })
+
+  var typed = new Typed('.typing', {
+    strings: [
+      "count your GPA",
+      "give you advice",
+      "help you study",
+      "suggest your scores",
+      "help you choose your major",
+    ],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true,
+  })
+
+  // this is really cool
+  // but you should vote Mikuni first
+
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items: 2,
+        nav: false
+      },
+    }
+  });
+
+})
